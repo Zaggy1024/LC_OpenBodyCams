863950001dd7cfaa6429fc22d1c1448b6baeff1f
Zaggy1024 <Zaggy1024@gmail.com>
2024-01-30 05:41:23 -0600

    Remove unnecessary debug logging in `ModelReplacementAPICompatibility`

--- OpenBodyCams/Compatibility/ModelReplacementAPICompatibility.cs
+++ OpenBodyCams/Compatibility/ModelReplacementAPICompatibility.cs
@@ -13,10 +13,7 @@ namespace OpenBodyCams.Compatibility
         public static GameObject[] CollectCosmetics(PlayerControllerB player)
         {
             if (player.GetComponentInChildren<BodyReplacementBase>() is BodyReplacementBase bodyReplacement)
-            {
-                Plugin.Instance.Logger.LogInfo($"Getting {player.playerUsername}'s model replacement");
                 return bodyReplacement.replacementModel.GetComponentsInChildren<Transform>().Select(cosmeticObject => cosmeticObject.gameObject).ToArray();
-            }
 
             return new GameObject[0];
         }
