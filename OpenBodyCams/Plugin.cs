3ca16f7cbf4bb194e84a4b9945a33bf0c9d92409
Zaggy1024 <Zaggy1024@gmail.com>
2024-02-13 14:12:25 -0600

    Use simplified new in Plugin

--- OpenBodyCams/Plugin.cs
+++ OpenBodyCams/Plugin.cs
@@ -26,7 +26,7 @@ namespace OpenBodyCams
         public const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
         public const string MOD_VERSION = "1.1.4";
 
-        private readonly Harmony harmony = new Harmony(MOD_UNIQUE_NAME);
+        private readonly Harmony harmony = new(MOD_UNIQUE_NAME);
 
         public static Plugin Instance { get; private set; }
 
