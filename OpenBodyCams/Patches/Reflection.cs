b9f6ec7d24d84256a8e92cb05c4c2c7a0572a4a1
Zaggy1024 <Zaggy1024@gmail.com>
2024-02-18 17:11:02 -0600

    Remove unused fields and use collection expressions in Reflection

--- OpenBodyCams/Patches/Reflection.cs
+++ OpenBodyCams/Patches/Reflection.cs
@@ -7,11 +7,7 @@ namespace OpenBodyCams.Patches
 {
     public static class Reflection
     {
-        public static readonly MethodInfo m_Debug_Log = typeof(Debug).GetMethod(nameof(Debug.Log), new Type[] { typeof(object) });
-
-        public static readonly MethodInfo m_GameObject_set_layer = typeof(GameObject).GetMethod("set_layer", new Type[] { typeof(int) });
-
-        public static readonly MethodInfo m_StartOfRound_get_Instance = typeof(StartOfRound).GetMethod("get_Instance", new Type[0]);
+        public static readonly MethodInfo m_StartOfRound_get_Instance = typeof(StartOfRound).GetMethod("get_Instance", []);
         public static readonly FieldInfo f_StartOfRound_thisClientPlayerId = typeof(StartOfRound).GetField(nameof(StartOfRound.thisClientPlayerId));
     }
 }
