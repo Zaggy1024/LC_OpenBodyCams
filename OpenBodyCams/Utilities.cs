a2fe24a210f1723a526d610ccabedb5b8f827361
Zaggy1024 <Zaggy1024@gmail.com>
2024-02-18 19:12:12 -0600

    Cache the renderer bounds and layer in `IsVisibleToAnyCameraExcept()`
    Getting both of these involves calling native wrapper getters, so let's avoid causing that overhead multiple times.

--- OpenBodyCams/Utilities.cs
+++ OpenBodyCams/Utilities.cs
@@ -8,17 +8,20 @@ namespace OpenBodyCams
 
         public static bool IsVisibleToAnyCameraExcept(this Renderer renderer, Camera cameraToSkip, bool debug = false)
         {
+            var bounds = renderer.bounds;
+            var layer = renderer.gameObject.layer;
+
             foreach (var camera in Camera.allCameras)
             {
                 if ((object)camera == cameraToSkip)
                     continue;
                 if (!camera.isActiveAndEnabled)
                     continue;
-                if ((camera.cullingMask & (1 << renderer.gameObject.layer)) == 0)
+                if ((camera.cullingMask & (1 << layer)) == 0)
                     continue;
 
                 GeometryUtility.CalculateFrustumPlanes(camera, frustumPlanes);
-                if (GeometryUtility.TestPlanesAABB(frustumPlanes, renderer.bounds))
+                if (GeometryUtility.TestPlanesAABB(frustumPlanes, bounds))
                     return true;
             }
 
