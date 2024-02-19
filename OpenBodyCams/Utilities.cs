85ef518ae50e671fbd62a4f8abb903dc7c57383a
Zaggy1024 <Zaggy1024@gmail.com>
2024-02-18 22:27:31 -0600

    Cache an array for the active cameras in `IsVisibleToAnyCameraExcept()`
    Avoids one more unnecessary allocation in the function.

--- OpenBodyCams/Utilities.cs
+++ OpenBodyCams/Utilities.cs
@@ -1,19 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace OpenBodyCams
 {
     public static class Utilities
     {
+        private static Camera[] allCameras = [];
         private static readonly Plane[] frustumPlanes = new Plane[6];
 
         public static bool IsVisibleToAnyCameraExcept(this Renderer renderer, Camera cameraToSkip, bool debug = false)
         {
+            if (allCameras.Length != Camera.allCamerasCount)
+                allCameras = new Camera[Camera.allCamerasCount];
+            Camera.GetAllCameras(allCameras);
+
             var bounds = renderer.bounds;
             var layer = renderer.gameObject.layer;
 
-            foreach (var camera in Camera.allCameras)
+            foreach (var camera in allCameras)
             {
-                if ((object)camera == cameraToSkip)
+                if (camera is not null && (object)camera == cameraToSkip)
                     continue;
                 if (!camera.isActiveAndEnabled)
                     continue;
