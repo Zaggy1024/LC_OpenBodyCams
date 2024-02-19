932deaeb855e351fcc357bae06612a0905ec52d8
Zaggy1024 <Zaggy1024@gmail.com>
2024-02-18 18:39:34 -0600

    Avoid GC churn when checking visibility of objects to cameras
    Allocating a `Plane[]` for two cameras in the scene every frame is totally unnecessary, we can just keep an array allocated instead.

--- OpenBodyCams/Utilities.cs
+++ OpenBodyCams/Utilities.cs
@@ -4,6 +4,8 @@ namespace OpenBodyCams
 {
     public static class Utilities
     {
+        private static readonly Plane[] frustumPlanes = new Plane[6];
+
         public static bool IsVisibleToAnyCameraExcept(this Renderer renderer, Camera cameraToSkip, bool debug = false)
         {
             foreach (var camera in Camera.allCameras)
@@ -15,7 +17,8 @@ namespace OpenBodyCams
                 if ((camera.cullingMask & (1 << renderer.gameObject.layer)) == 0)
                     continue;
 
-                if (GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(camera), renderer.bounds))
+                GeometryUtility.CalculateFrustumPlanes(camera, frustumPlanes);
+                if (GeometryUtility.TestPlanesAABB(frustumPlanes, renderer.bounds))
                     return true;
             }
 
