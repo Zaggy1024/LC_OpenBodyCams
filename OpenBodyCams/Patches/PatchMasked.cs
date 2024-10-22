25e518cda73ee8abf21309c0a97fcbd523553059
Zaggy1024 <Zaggy1024@gmail.com>
2024-10-22 08:31:17 -0500

    Use a namespace declaration in PatchMasked

--- OpenBodyCams/Patches/PatchMasked.cs
+++ OpenBodyCams/Patches/PatchMasked.cs
@@ -2,49 +2,48 @@
 
 using HarmonyLib;
 
-namespace OpenBodyCams.Patches
+namespace OpenBodyCams.Patches;
+
+[HarmonyPatch(typeof(HauntedMaskItem))]
+internal static class PatchHauntedMaskItem
 {
-    [HarmonyPatch(typeof(HauntedMaskItem))]
-    internal static class PatchHauntedMaskItem
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(HauntedMaskItem.CreateMimicServerRpc))]
+    private static void CreateMimicServerRpcPostfix()
     {
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(HauntedMaskItem.CreateMimicServerRpc))]
-        private static void CreateMimicServerRpcPostfix()
-        {
-            BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch("waitForMimicEnemySpawn")]
-        private static IEnumerator waitForMimicEnemySpawnPostfix(IEnumerator __result)
-        {
-            while (__result.MoveNext())
-                yield return __result.Current;
-
-            BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
-        }
+        BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
     }
 
-    [HarmonyPatch(typeof(MaskedPlayerEnemy))]
-    internal static class PatchMaskedPlayerEnemy
+    [HarmonyPostfix]
+    [HarmonyPatch("waitForMimicEnemySpawn")]
+    private static IEnumerator waitForMimicEnemySpawnPostfix(IEnumerator __result)
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(MaskedPlayerEnemy.FinishKillAnimation))]
-        private static void FinishKillAnimationPrefix(bool __0)
-        {
-            var killedPlayer = __0;
-            if (killedPlayer)
-                BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
-        }
+        while (__result.MoveNext())
+            yield return __result.Current;
 
-        [HarmonyPostfix]
-        [HarmonyPatch("waitForMimicEnemySpawn")]
-        private static IEnumerator waitForMimicEnemySpawnPostfix(IEnumerator __result)
-        {
-            while (__result.MoveNext())
-                yield return __result.Current;
+        BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
+    }
+}
 
+[HarmonyPatch(typeof(MaskedPlayerEnemy))]
+internal static class PatchMaskedPlayerEnemy
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(MaskedPlayerEnemy.FinishKillAnimation))]
+    private static void FinishKillAnimationPrefix(bool __0)
+    {
+        var killedPlayer = __0;
+        if (killedPlayer)
             BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
-        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("waitForMimicEnemySpawn")]
+    private static IEnumerator waitForMimicEnemySpawnPostfix(IEnumerator __result)
+    {
+        while (__result.MoveNext())
+            yield return __result.Current;
+
+        BodyCamComponent.MarkTargetStatusChangedForAllBodyCams();
     }
 }
