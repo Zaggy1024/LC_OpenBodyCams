6cf6a3d0d879c96c3ae53366f488d0ce3b227da8
Zaggy1024 <Zaggy1024@gmail.com>
2024-10-22 11:40:21 -0500

    Rename matcher to injector in PatchHDRenderPipeline

--- OpenBodyCams/Patches/PatchHDRenderPipeline.cs
+++ OpenBodyCams/Patches/PatchHDRenderPipeline.cs
@@ -23,23 +23,23 @@ internal static class PatchHDRenderPipeline
     [HarmonyPatch(nameof(HDRenderPipeline.Render), [typeof(ScriptableRenderContext), typeof(List<Camera>)])]
     private static IEnumerable<CodeInstruction> RenderTranspiler(IEnumerable<CodeInstruction> instructions)
     {
-        var matcher = new ILInjector(instructions);
+        var injector = new ILInjector(instructions);
 
         // + BeforeCameraCullingHook(camera, context);
         //   if (PrepareAndCullCamera(camera, xrPass, flag, value, renderContext, out var renderRequest))
-        matcher.Reset()
+        injector.Reset()
             .FindStart([
                 ILMatcher.Call(typeof(HDRenderPipeline).GetMethod(nameof(HDRenderPipeline.PrepareAndCullCamera), BindingFlags.NonPublic | BindingFlags.Instance, [typeof(Camera), typeof(XRPass), typeof(bool), typeof(List<HDRenderPipeline.RenderRequest>), typeof(ScriptableRenderContext), typeof(HDRenderPipeline.RenderRequest).MakeByRefType(), typeof(CubemapFace)])),
             ])
             .GoToPush(6)
             .Forward(1);
 
-        if (!matcher.IsValid)
+        if (!injector.IsValid)
         {
             Plugin.Instance.Logger.LogError($"Failed to find the HDRP culling loop");
             return instructions;
         }
-        matcher
+        injector
             .InsertInPlace([
                 new(OpCodes.Dup),
                 new(OpCodes.Ldarg_1),
@@ -51,7 +51,7 @@ internal static class PatchHDRenderPipeline
         //   RenderRequest request = flattenedRequests[requestIndex];
         //   ..
         //   ExecuteRenderRequest(request, context, commandBuffer, AOVRequestData.defaultAOVRequestDataNonAlloc);
-        matcher
+        injector
             .FindEnd([
                 ILMatcher.Ldloc(),
                 ILMatcher.Ldloc(),
@@ -59,20 +59,20 @@ internal static class PatchHDRenderPipeline
                 ILMatcher.Stloc(),
             ]);
 
-        if (!matcher.IsValid)
+        if (!injector.IsValid)
         {
             Plugin.Instance.Logger.LogError($"Failed to find the HDRP render loop");
             return instructions;
         }
 
-        matcher.Back(1)
+        injector.Back(1)
             .Insert([
                 new(OpCodes.Dup),
                 new(OpCodes.Ldarg_1),
                 new(OpCodes.Call, typeof(PatchHDRenderPipeline).GetMethod(nameof(BeforeCameraRenderingHook), BindingFlags.NonPublic | BindingFlags.Static, [typeof(HDRenderPipeline.RenderRequest), typeof(ScriptableRenderContext)])),
             ]);
 
-        return matcher.ReleaseInstructions();
+        return injector.ReleaseInstructions();
     }
 
     private static void BeforeCameraCullingHook(Camera camera, ScriptableRenderContext context)
