b4ae692d1731f807827c54805f13bfca6f5b978e
Zaggy1024 <Zaggy1024@gmail.com>
2024-11-01 03:30:27 -0500

    Use namespace declarations in the Overlay namespace

--- OpenBodyCams/Overlay/OverlayManager.cs
+++ OpenBodyCams/Overlay/OverlayManager.cs
@@ -6,136 +6,135 @@ using UnityEngine.Rendering.HighDefinition;
 
 using OpenBodyCams.Utilities;
 
-namespace OpenBodyCams.Overlay
+namespace OpenBodyCams.Overlay;
+
+internal class OverlayManager : MonoBehaviour
 {
-    internal class OverlayManager : MonoBehaviour
+    internal BodyCamComponent BodyCam;
+
+    private Camera camera;
+
+    private Canvas canvas;
+    private TextMeshProUGUI textRenderer;
+
+    private float baseFontSize;
+    private Vector4 baseMargin;
+
+    private bool renderThisFrame = false;
+
+    private void Start()
     {
-        internal BodyCamComponent BodyCam;
+        camera = GetComponentInChildren<Camera>();
 
-        private Camera camera;
+        var volume = camera.gameObject.AddComponent<CustomPassVolume>();
+        volume.targetCamera = camera;
 
-        private Canvas canvas;
-        private TextMeshProUGUI textRenderer;
-
-        private float baseFontSize;
-        private Vector4 baseMargin;
-
-        private bool renderThisFrame = false;
-
-        private void Start()
+        var pass = (TransparentRenderTexturePass)volume.AddPassOfType<TransparentRenderTexturePass>();
+        pass.targetColorBuffer = CustomPass.TargetBuffer.Custom;
+        pass.targetDepthBuffer = CustomPass.TargetBuffer.Custom;
+        pass.clearFlags = UnityEngine.Rendering.ClearFlag.All;
+        pass.targetTexture = camera.targetTexture;
+        camera.targetTexture = new RenderTexture(camera.targetTexture)
         {
-            camera = GetComponentInChildren<Camera>();
+            name = "Dummy Overlay Camera Output",
+        };
 
-            var volume = camera.gameObject.AddComponent<CustomPassVolume>();
-            volume.targetCamera = camera;
+        var hdCamera = camera.GetComponent<HDAdditionalCameraData>();
+        hdCamera.customRenderingSettings = true;
+        hdCamera.renderingPathCustomFrameSettingsOverrideMask.mask[(uint)FrameSettingsField.DecalLayers] = true;
+        hdCamera.renderingPathCustomFrameSettings.SetEnabled(FrameSettingsField.DecalLayers, false);
 
-            var pass = (TransparentRenderTexturePass)volume.AddPassOfType<TransparentRenderTexturePass>();
-            pass.targetColorBuffer = CustomPass.TargetBuffer.Custom;
-            pass.targetDepthBuffer = CustomPass.TargetBuffer.Custom;
-            pass.clearFlags = UnityEngine.Rendering.ClearFlag.All;
-            pass.targetTexture = camera.targetTexture;
-            camera.targetTexture = new RenderTexture(camera.targetTexture)
-            {
-                name = "Dummy Overlay Camera Output",
-            };
+        canvas = GetComponentInChildren<Canvas>();
 
-            var hdCamera = camera.GetComponent<HDAdditionalCameraData>();
-            hdCamera.customRenderingSettings = true;
-            hdCamera.renderingPathCustomFrameSettingsOverrideMask.mask[(uint)FrameSettingsField.DecalLayers] = true;
-            hdCamera.renderingPathCustomFrameSettings.SetEnabled(FrameSettingsField.DecalLayers, false);
+        textRenderer = canvas.GetComponentInChildren<TextMeshProUGUI>();
+        textRenderer.font = StartOfRound.Instance.screenLevelDescription.font;
+        baseFontSize = textRenderer.fontSize;
+        baseMargin = textRenderer.margin;
 
-            canvas = GetComponentInChildren<Canvas>();
+        BodyCam.OnCameraStatusChanged += BodyCamStatusChanged;
+        BodyCam.OnScreenPowerChanged += BodyCamScreenPowerChanged;
+        API.BodyCam.OnBodyCamReceiverBecameEnabled += UpdateText;
+        API.BodyCam.OnBodyCamReceiverBecameDisabled += UpdateText;
 
-            textRenderer = canvas.GetComponentInChildren<TextMeshProUGUI>();
-            textRenderer.font = StartOfRound.Instance.screenLevelDescription.font;
-            baseFontSize = textRenderer.fontSize;
-            baseMargin = textRenderer.margin;
+        CreateOverlayMesh();
 
-            BodyCam.OnCameraStatusChanged += BodyCamStatusChanged;
-            BodyCam.OnScreenPowerChanged += BodyCamScreenPowerChanged;
-            API.BodyCam.OnBodyCamReceiverBecameEnabled += UpdateText;
-            API.BodyCam.OnBodyCamReceiverBecameDisabled += UpdateText;
+        UpdatePreferences();
+        UpdateText();
+    }
 
-            CreateOverlayMesh();
+    private void CreateOverlayMesh()
+    {
+        var overlayObject = Instantiate(Plugin.Assets.LoadAsset<GameObject>("Assets/OpenBodyCams/Prefabs/BodyCamOverlayMesh.prefab"));
 
-            UpdatePreferences();
-            UpdateText();
+        var overlayTransform = overlayObject.transform;
+        var copyTransform = BodyCam.MonitorRenderer.transform;
+        overlayTransform.parent = copyTransform.parent;
+        overlayTransform.localScale = copyTransform.localScale;
+        overlayTransform.SetLocalPositionAndRotation(copyTransform.localPosition, copyTransform.localRotation);
+
+        var overlayMeshFilter = overlayObject.GetComponent<MeshFilter>();
+        overlayMeshFilter.mesh = MeshUtils.CopySubmesh(BodyCam.MonitorRenderer.GetComponent<MeshFilter>().mesh, BodyCam.MonitorMaterialIndex);
+    }
+
+    private void Update()
+    {
+        camera.enabled = renderThisFrame;
+        renderThisFrame = false;
+    }
+
+    internal void UpdatePreferences()
+    {
+        textRenderer.fontSize = baseFontSize * Plugin.OverlayTextScale.Value;
+        textRenderer.margin = baseMargin * Plugin.OverlayTextScale.Value;
+
+        renderThisFrame = true;
+    }
+
+    private void BodyCamStatusChanged(CameraRenderingStatus _)
+    {
+        UpdateText();
+    }
+
+    private void BodyCamScreenPowerChanged(bool _)
+    {
+        UpdateText();
+    }
+
+    internal void UpdateText()
+    {
+        canvas.gameObject.SetActive(BodyCam.IsScreenPowered());
+
+        textRenderer.text = GetText();
+        textRenderer.enabled = textRenderer.text != "";
+
+        renderThisFrame = true;
+    }
+
+    private string GetText()
+    {
+        if (ShipUpgrades.BodyCamUnlockable != null)
+        {
+            if (!ShipUpgrades.BodyCamUnlockable.hasBeenUnlockedByPlayer)
+                return Plugin.BuyAntennaText.Value.Replace("{price}", ShipUpgrades.BodyCamPrice.ToString(), StringComparison.OrdinalIgnoreCase);
+
+            if (!ShipUpgrades.BodyCamUnlockableIsPlaced)
+                return Plugin.AntennaStoredText.Value;
         }
 
-        private void CreateOverlayMesh()
+        return BodyCam.CameraStatus switch
         {
-            var overlayObject = Instantiate(Plugin.Assets.LoadAsset<GameObject>("Assets/OpenBodyCams/Prefabs/BodyCamOverlayMesh.prefab"));
+            CameraRenderingStatus.Rendering => Plugin.DefaultText.Value,
+            CameraRenderingStatus.TargetInvalid => Plugin.TargetInvalidText.Value,
+            CameraRenderingStatus.TargetDisabledOnShip => Plugin.TargetOnShipText.Value,
+            _ => "",
+        };
+    }
 
-            var overlayTransform = overlayObject.transform;
-            var copyTransform = BodyCam.MonitorRenderer.transform;
-            overlayTransform.parent = copyTransform.parent;
-            overlayTransform.localScale = copyTransform.localScale;
-            overlayTransform.SetLocalPositionAndRotation(copyTransform.localPosition, copyTransform.localRotation);
-
-            var overlayMeshFilter = overlayObject.GetComponent<MeshFilter>();
-            overlayMeshFilter.mesh = MeshUtils.CopySubmesh(BodyCam.MonitorRenderer.GetComponent<MeshFilter>().mesh, BodyCam.MonitorMaterialIndex);
-        }
-
-        private void Update()
-        {
-            camera.enabled = renderThisFrame;
-            renderThisFrame = false;
-        }
-
-        internal void UpdatePreferences()
-        {
-            textRenderer.fontSize = baseFontSize * Plugin.OverlayTextScale.Value;
-            textRenderer.margin = baseMargin * Plugin.OverlayTextScale.Value;
-
-            renderThisFrame = true;
-        }
-
-        private void BodyCamStatusChanged(CameraRenderingStatus _)
-        {
-            UpdateText();
-        }
-
-        private void BodyCamScreenPowerChanged(bool _)
-        {
-            UpdateText();
-        }
-
-        internal void UpdateText()
-        {
-            canvas.gameObject.SetActive(BodyCam.IsScreenPowered());
-
-            textRenderer.text = GetText();
-            textRenderer.enabled = textRenderer.text != "";
-
-            renderThisFrame = true;
-        }
-
-        private string GetText()
-        {
-            if (ShipUpgrades.BodyCamUnlockable != null)
-            {
-                if (!ShipUpgrades.BodyCamUnlockable.hasBeenUnlockedByPlayer)
-                    return Plugin.BuyAntennaText.Value.Replace("{price}", ShipUpgrades.BodyCamPrice.ToString(), StringComparison.OrdinalIgnoreCase);
-
-                if (!ShipUpgrades.BodyCamUnlockableIsPlaced)
-                    return Plugin.AntennaStoredText.Value;
-            }
-
-            return BodyCam.CameraStatus switch
-            {
-                CameraRenderingStatus.Rendering => Plugin.DefaultText.Value,
-                CameraRenderingStatus.TargetInvalid => Plugin.TargetInvalidText.Value,
-                CameraRenderingStatus.TargetDisabledOnShip => Plugin.TargetOnShipText.Value,
-                _ => "",
-            };
-        }
-
-        private void OnDestroy()
-        {
-            BodyCam.OnCameraStatusChanged -= BodyCamStatusChanged;
-            BodyCam.OnScreenPowerChanged -= BodyCamScreenPowerChanged;
-            API.BodyCam.OnBodyCamReceiverBecameEnabled -= UpdateText;
-            API.BodyCam.OnBodyCamReceiverBecameDisabled -= UpdateText;
-        }
+    private void OnDestroy()
+    {
+        BodyCam.OnCameraStatusChanged -= BodyCamStatusChanged;
+        BodyCam.OnScreenPowerChanged -= BodyCamScreenPowerChanged;
+        API.BodyCam.OnBodyCamReceiverBecameEnabled -= UpdateText;
+        API.BodyCam.OnBodyCamReceiverBecameDisabled -= UpdateText;
     }
 }
--- OpenBodyCams/Overlay/TransparentRenderTexturePass.cs
+++ OpenBodyCams/Overlay/TransparentRenderTexturePass.cs
@@ -3,68 +3,67 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Rendering.RendererUtils;
 
-namespace OpenBodyCams.Overlay
+namespace OpenBodyCams.Overlay;
+
+public class TransparentRenderTexturePass : CustomPass
 {
-    public class TransparentRenderTexturePass : CustomPass
+    public RenderTexture targetTexture;
+
+    private static ShaderTagId[] depthPrepassTags;
+    private static ShaderTagId[] forwardTags;
+
+    public override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
     {
-        public RenderTexture targetTexture;
+        depthPrepassTags = [
+            HDShaderPassNames.s_DepthForwardOnlyName,
+            HDShaderPassNames.s_ForwardOnlyName,
+            HDShaderPassNames.s_DepthOnlyName,
+            HDShaderPassNames.s_TransparentDepthPrepassName,
+            HDShaderPassNames.s_TransparentBackfaceName,
+        ];
+        forwardTags = [
+            HDShaderPassNames.s_ForwardOnlyName,
+            HDShaderPassNames.s_ForwardName,
+            HDShaderPassNames.s_SRPDefaultUnlitName,
+            HDShaderPassNames.s_DecalMeshForwardEmissiveName,
+        ];
+    }
 
-        private static ShaderTagId[] depthPrepassTags;
-        private static ShaderTagId[] forwardTags;
-
-        public override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
+    private void Render(ref CustomPassContext ctx, in RenderQueueRange range, SortingCriteria sorting, ShaderTagId[] shaderTags, PerObjectData configuration, bool fptl)
+    {
+        var camera = ctx.hdCamera.camera;
+        RendererList rendererList = ctx.renderContext.CreateRendererList(new RendererListDesc(shaderTags, ctx.cullingResults, camera)
         {
-            depthPrepassTags = [
-                HDShaderPassNames.s_DepthForwardOnlyName,
-                HDShaderPassNames.s_ForwardOnlyName,
-                HDShaderPassNames.s_DepthOnlyName,
-                HDShaderPassNames.s_TransparentDepthPrepassName,
-                HDShaderPassNames.s_TransparentBackfaceName,
-            ];
-            forwardTags = [
-                HDShaderPassNames.s_ForwardOnlyName,
-                HDShaderPassNames.s_ForwardName,
-                HDShaderPassNames.s_SRPDefaultUnlitName,
-                HDShaderPassNames.s_DecalMeshForwardEmissiveName,
-            ];
-        }
+            renderQueueRange = range,
+            rendererConfiguration = configuration,
+            sortingCriteria = sorting,
+            excludeObjectMotionVectors = false,
+            layerMask = camera.cullingMask,
+        });
+        CoreUtils.SetKeyword(ctx.cmd, "USE_FPTL_LIGHTLIST", fptl);
+        CoreUtils.SetKeyword(ctx.cmd, "USE_CLUSTERED_LIGHTLIST", !fptl);
+        CoreUtils.DrawRendererList(ctx.renderContext, ctx.cmd, rendererList);
+    }
 
-        private void Render(ref CustomPassContext ctx, in RenderQueueRange range, SortingCriteria sorting, ShaderTagId[] shaderTags, PerObjectData configuration, bool fptl)
-        {
-            var camera = ctx.hdCamera.camera;
-            RendererList rendererList = ctx.renderContext.CreateRendererList(new RendererListDesc(shaderTags, ctx.cullingResults, camera)
-            {
-                renderQueueRange = range,
-                rendererConfiguration = configuration,
-                sortingCriteria = sorting,
-                excludeObjectMotionVectors = false,
-                layerMask = camera.cullingMask,
-            });
-            CoreUtils.SetKeyword(ctx.cmd, "USE_FPTL_LIGHTLIST", fptl);
-            CoreUtils.SetKeyword(ctx.cmd, "USE_CLUSTERED_LIGHTLIST", !fptl);
-            CoreUtils.DrawRendererList(ctx.renderContext, ctx.cmd, rendererList);
-        }
+    private bool ShouldUseFPTL(in FrameSettings frameSettings)
+    {
+        if (frameSettings.litShaderMode == LitShaderMode.Deferred)
+            return true;
+        return frameSettings.IsEnabled(FrameSettingsField.FPTLForForwardOpaque);
+    }
 
-        private bool ShouldUseFPTL(in FrameSettings frameSettings)
-        {
-            if (frameSettings.litShaderMode == LitShaderMode.Deferred)
-                return true;
-            return frameSettings.IsEnabled(FrameSettingsField.FPTLForForwardOpaque);
-        }
+    public override void Execute(CustomPassContext ctx)
+    {
+        ctx.cmd.SetRenderTarget(targetTexture.colorBuffer, targetTexture.depthBuffer);
+        ctx.cmd.ClearRenderTarget(true, true, Color.clear);
 
-        public override void Execute(CustomPassContext ctx)
-        {
-            ctx.cmd.SetRenderTarget(targetTexture.colorBuffer, targetTexture.depthBuffer);
-            ctx.cmd.ClearRenderTarget(true, true, Color.clear);
+        var frameSettings = ctx.hdCamera.frameSettings;
+        PerObjectData rendererConfiguration = HDUtils.GetRendererConfiguration(frameSettings.IsEnabled(FrameSettingsField.ProbeVolume), frameSettings.IsEnabled(FrameSettingsField.Shadowmask));
 
-            var frameSettings = ctx.hdCamera.frameSettings;
-            PerObjectData rendererConfiguration = HDUtils.GetRendererConfiguration(frameSettings.IsEnabled(FrameSettingsField.ProbeVolume), frameSettings.IsEnabled(FrameSettingsField.Shadowmask));
+        Render(ref ctx, RenderQueueRange.opaque, SortingCriteria.CommonOpaque, depthPrepassTags, rendererConfiguration, true);
 
-            Render(ref ctx, RenderQueueRange.opaque, SortingCriteria.CommonOpaque, depthPrepassTags, rendererConfiguration, true);
+        Render(ref ctx, RenderQueueRange.opaque, SortingCriteria.CommonOpaque, forwardTags, rendererConfiguration, ShouldUseFPTL(frameSettings));
 
-            Render(ref ctx, RenderQueueRange.opaque, SortingCriteria.CommonOpaque, forwardTags, rendererConfiguration, ShouldUseFPTL(frameSettings));
-
-            Render(ref ctx, RenderQueueRange.transparent, SortingCriteria.CommonTransparent, forwardTags, rendererConfiguration, false);
-        }
+        Render(ref ctx, RenderQueueRange.transparent, SortingCriteria.CommonTransparent, forwardTags, rendererConfiguration, false);
     }
 }
